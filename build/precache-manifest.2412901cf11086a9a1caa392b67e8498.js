6cac35ee22242c8a1dca8b0124a911ec8475a7ad
ZeroVirusEXE <ZeroVirusEXE@gmail.com>
2019-10-17 17:29:48 -0400

    initial commit

---
+++ build/precache-manifest.2412901cf11086a9a1caa392b67e8498.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5266f29aadcd18d2e00a79ac81965ced",
+    "url": "/culinary/index.html"
+  },
+  {
+    "revision": "b846a442b14e7aa63027",
+    "url": "/culinary/static/js/2.3a790653.chunk.js"
+  },
+  {
+    "revision": "aec00918c3de2f6449e3",
+    "url": "/culinary/static/js/main.3d5fb6af.chunk.js"
+  },
+  {
+    "revision": "b2012a7b55309196ea49",
+    "url": "/culinary/static/js/runtime-main.24146e92.js"
+  },
+  {
+    "revision": "cb6ba5e8988a6c5d51f97227dd9c4d13",
+    "url": "/culinary/static/media/Culinary-Logo-Text.cb6ba5e8.png"
+  }
+]);
